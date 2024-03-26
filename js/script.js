e8a28dc8d365fac2afc611cd141a3238b2f7f599
ryampeguero <ryampeguero@gmail.com>
2024-03-26 23:51:24 +0100

    feat: js file  done

--- js/script.js
+++ js/script.js
@@ -2,27 +2,39 @@
 let price;
 
 //Raccolta dati
-const kilometer = parseInt(prompt("Quanti chilometri vuoi percorrere?")); // number
-const age = parseInt(prompt("Quanti anni hai?")); // number
+const fullname = document.getElementById("fullname"); // string | null
+const kilometer = document.getElementById("kilometer");  // number | null
+const age = document.getElementById("age"); // number | null
 
-if (!isNaN(kilometer) && !isNaN(age) && age > 0 && kilometer > 0 && age < 110) {
+const btnForm = document.getElementById("invia");
+
+btnForm.addEventListener("click", function(){
+    console.log(parseInt(age.value));
+    console.log(parseInt(kilometer.value));
     
-    //Esecuzione logica
-    price = kilometer * 0.21; // number
-    console.log(price);
-    if (age < 18) {
-        price = price - (price * 0.20);
-        document.getElementById("age").innerHTML = "Minorenne:"
-    } else if (age >= 65) {
-        price = price - (price * 0.40);  
-        document.getElementById("age").innerHTML = "Over 65:" 
+    const kilometerInt = parseInt(kilometer.value);
+    const ageInt = parseInt(age.value);
+    if (!isNaN(kilometerInt) && !isNaN(ageInt) && ageInt > 0 && kilometerInt > 0 && ageInt < 110) {
+    
+        //Esecuzione logica
+        price = kilometerInt * 0.21; // number
+        console.log(price);
+        let discount = 0;
+        
+        if (ageInt < 18) {
+            discount = 0.20;
+            // document.getElementById("age").innerHTML = "Minorenne:"
+        } else if (ageInt >= 65) {
+            discount = 0.40;
+            // document.getElementById("age").innerHTML = "Over 65:" 
+        }
+
+        price = price - (price * discount);
+        //Output
+        console.log("discount:", discount)
+        console.log("price:", price)
+        // document.getElementById("biglietto").innerHTML = price.toFixed(2);
+    } else {
+        alert ("Dati errati!!")
     }
-
-    //Output
-
-    document.getElementById("biglietto").innerHTML = price.toFixed(2);
-} else {
-    alert ("Dati errati!!")
-}
-
-document.getElementByd("biglietto").addEventListener("click", );
+})
